ace7f0b690bddf78d171c7bfdde9be665d0cb81f
Francium6623 <110833307+Mccandycube6623@users.noreply.github.com>
2026-09-20 00:18:55 +0800

    [i]

--- src/main/java/gd/rf/kongzhongtitian/PipeAPI/TransporterNodeMenu.java
+++ src/main/java/gd/rf/kongzhongtitian/PipeAPI/TransporterNodeMenu.java
@@ -20,6 +20,8 @@ public class TransporterNodeMenu extends AbstractContainerMenu {
     public static final int RESERVED_X = 116, RESERVED_Y = 50;
     //public static final int DIR_Y = 96;
     //public static final int[] DIR_X = {26, 44, 62, 80, 98, 116};
+    public static final int[] DIR_X = {20, 38, 20, 2, 20, 38};
+	public static final int[] DIR_Y = {152, 134, 116, 134, 134, 152};
 
     // 玩家背包起始 Y
     public static final int INV_Y = 128;
@@ -44,16 +46,14 @@ public class TransporterNodeMenu extends AbstractContainerMenu {
         this.addSlot(new LockedSlot(nodeInventory,
                 TransporterNodeBlockEntity.SLOT_RESERVED, RESERVED_X, RESERVED_Y));
         // 方向槽：E S W N U D
-        this.addSlot(new RedstoneTorchSlot(nodeInventory, SLOT_DIR_START + 0, 20, 152));
-		this.addSlot(new RedstoneTorchSlot(nodeInventory, SLOT_DIR_START + 1, 38, 134));
-		this.addSlot(new RedstoneTorchSlot(nodeInventory, SLOT_DIR_START + 2, 20, 116));
-		this.addSlot(new RedstoneTorchSlot(nodeInventory, SLOT_DIR_START + 3, 2, 134));
-		this.addSlot(new RedstoneTorchSlot(nodeInventory, SLOT_DIR_START + 4, 20, 134));
-		this.addSlot(new RedstoneTorchSlot(nodeInventory, SLOT_DIR_START + 5, 38, 152));
         //for (int i = 0; i < TransporterNodeBlockEntity.DIR_SLOT_COUNT; i++) {
         //    this.addSlot(new RedstoneTorchSlot(nodeInventory,
         //            TransporterNodeBlockEntity.SLOT_DIR_START + i, DIR_X[i], DIR_Y));
         //}
+        for (int i = 0; i < TransporterNodeBlockEntity.DIR_SLOT_COUNT; i++) {
+            this.addSlot(new RedstoneTorchSlot(nodeInventory,
+                    TransporterNodeBlockEntity.SLOT_DIR_START + i, DIR_X[i], DIR_Y[i]));
+        }
 
         // 玩家背包 3 行
         for (int row = 0; row < 3; ++row) {
